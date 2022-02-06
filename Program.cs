0b638ca4eda487a316139d7a21f0a184b46f855b
Maciej D <mcjdzi@gmail.com>
2022-02-06 15:44:16 +0100

    init

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace memory_game
+{
+	class Program
+	{
+		static int Main(string[] args)
+		{
+			Console.WriteLine("Welcome to the memory game!");
+			do
+			{
+				Console.WriteLine("1. Play Game\n2. Exit");
+				ConsoleKeyInfo input = Console.ReadKey();
+				if (input.KeyChar == '1') //Start game
+				{
+					Console.WriteLine("playing game");
+				} 
+				else if(input.KeyChar == '2') //Exit game
+				{
+					return 0;
+				}
+				else
+				{
+					Console.WriteLine("\nEnter 1 or 2");
+				}
+			} while (true);
+		}
+	}
+}
